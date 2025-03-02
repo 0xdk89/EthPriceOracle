7623e8cc4082daa337a3e5a19a74e038494fdda0
DK <nerodk89@gmail.com>
2025-03-02 19:47:36 +0700

    Oracle contracts

---
+++ caller/contracts/EthPriceOracleInterface.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.5.0 <=0.9.0;
+
+abstract contract EthPriceOracleInterface {
+
+  function getLatestEthPrice() public virtual returns (uint256);
+  
+}
